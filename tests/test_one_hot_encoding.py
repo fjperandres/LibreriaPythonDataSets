811e60eebb6aeb963fc571de0e6350dd3af86cf2
Paumila <pau.milaguasch@gmail.com>
2022-11-18 20:11:39 +0100

    Update test_one_hot_encoding.py

--- tests/test_one_hot_encoding.py
+++ tests/test_one_hot_encoding.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from typing import Sequence, Dirt, Optional, Tuple, bool
+from typing import Sequence, Dict, Optional, Tuple, bool
 
 def one_hot_encoding(df: pd.DataFrame,col_list: Sequence[str],keep: bool=False) -> pd.DataFrame:
   for i in range(len(col_list)):
