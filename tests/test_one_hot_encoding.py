4b34d916042009084b9fb9c4197284e0a20ef12f
Paumila <pau.milaguasch@gmail.com>
2022-11-18 20:56:33 +0100

    one_hot_encoding

--- tests/test_one_hot_encoding.py
+++ tests/test_one_hot_encoding.py
@@ -1,14 +1,1 @@
-import pandas as pd
-from typing import Sequence, Dict, Optional, Tuple, bool
-
-def one_hot_encoding(df: pd.DataFrame,col_list: Sequence[str],keep: bool=False) -> pd.DataFrame:
-  for i in range(len(col_list)):
-    a = col_list[i]
-    if keep == False:
-      df = df.join(pd.get_dummies(df[str(a)], prefix= str(a)), how='left')
-    elif keep == True:
-      df = df.join(pd.get_dummies(df[str(a)], prefix= str(a)), how='left')
-      del(df[str(a)])
-  return df
-
 
