044b254e58d99cbc488fee33bd8c14979a420545
Xènia Domènech <xenia.domenech16@gmail.com>
2022-11-18 20:28:02 +0100

    Update test_fill_null.py

--- tests/test_fill_null.py
+++ tests/test_fill_null.py
@@ -7,10 +7,13 @@ from typing import Sequence, Dict, Optional, Tuple
 def fill_na (df: pd.DataFrame, method, column_list: Sequence[str]) -> pd.DataFrame :
     
     if method == 'Drop':
-        df = df[df['Column'].notna()]
+        df = df[df[column_list].notna()]
     elif method == 'Mean':
-        df = df[df['Column'].notna()]
+        if df.dtypes(column_list).contains('int') or df.dtypes(column_list).contains('float'):
+            df = df.loc[df[column_list].isnull(), column_list]= df[column_list].mean()
+        else:
+            print('No se ha podido aplicar el método porque la columna no es numérica.')
     else:
-        print('Método erroneo. Solo hay las opciones Mean o Drop')
+        print('Método erroneo. Solo hay las opciones Mean y Drop')
 
     return df
